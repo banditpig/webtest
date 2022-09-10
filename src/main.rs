1e219d86a1b16c7d16cb959b4ef31b2355640ffb
banditpig <mike_k_houghton@yahoo.co.uk>
2022-09-10 17:05:12 +0100

    3.8.5.2.2 Reading A Configuration File

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::net::TcpListener;
-use webtest::run;
-
-
+use webtest::configuration::get_configuration;
+use webtest::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
-
 }
