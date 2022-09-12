693cdb98383ab82ea003ab2baa2a134236cb8afe
banditpig <mike_k_houghton@yahoo.co.uk>
2022-09-12 17:13:55 +0100

    Start 4.5.11 Logs For Integration Tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 use webtest::configuration::get_configuration;
 use webtest::startup::run;
+use webtest::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("webtest".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
